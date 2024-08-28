e4335e43aeafc5011b0e3f62e4643ab3e2415c74
vananz123 <vanboan450@gmail.com>
2024-08-28 12:46:11 +0700

    update router

--- src/routers/upload/index.ts
+++ src/routers/upload/index.ts
@@ -5,7 +5,7 @@ import { asyncHandler } from '../../helpers/asyncHandler'
 import { uploadMemory } from '@/configs/multer.config'
 import uploadController from '@/controllers/upload.controller'
 //admin
-router.post('/blog/thumb', uploadMemory.single('file') , asyncHandler(uploadController.uploadImageFromLocalS3))
+router.post('/image', uploadMemory.single('file') , asyncHandler(uploadController.uploadImageFromLocalS3))
 //user
 //router.use(authentication)
 export default router
